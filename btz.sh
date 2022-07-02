009655ea5dc659b58a5909fb5fc0f2e7b7ec2e9a
linuxCowboy <lxc797@gmail.com>
2022-07-02 09:33:35 +0200

    fix u 1:2

--- btz.sh
+++ btz.sh
@@ -81,10 +81,10 @@ Ftimer ()
         # countup
         elif [[ $1 =~ ^u ]]; then
                 local HOT=0
-                [[ $2 =~ ^((((([0-9]*):)?)(([0-9]*):)?)([0-9]*))$ ]] &&
-                        HOT=$(( 10#${BASH_REMATCH[5]:-0} * 3600 +
-                                10#${BASH_REMATCH[7]:-0} * 60 +
-                                10#${BASH_REMATCH[8]:-0})) &&
+                [[ $2 =~ ^((([0-9]*):)?([0-9]*):)?([0-9]*)$ ]] &&
+                        HOT=$(( 10#${BASH_REMATCH[3]:-0} * 3600 +
+                                10#${BASH_REMATCH[4]:-0} * 60 +
+                                10#${BASH_REMATCH[5]:-0})) &&
                         shift
 
                 local START=$HOT
